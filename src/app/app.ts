9cae09ea425834e3fa59f974ba4d67dfa0fdd2c6
Rina-ui <marinagracia895@gmail.com>
2025-12-27 06:38:11 +0000

    modification

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Home} from './home/home';
+imports: [Home];
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<h1>Hello World!</h1>',
-  // templateUrl: './app.html',
+  imports: [RouterOutlet, Home],
+  template: '<main> <header class = "brand main"> <img class = "brand logo" src = "/assets/logo.svg" alt = "iamge" aria-hidden = "true"> <section class = "brand sec"> <app-home/> </section> </header> </main>',
+  // templatehomeUrl: './app.html',
   // styleUrl: './app.css'
 })
 export class App {
