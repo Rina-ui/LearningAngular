dabc1b3f1ab8259c28d8f37f00d13e4a1fddf3e3
Rina-ui <marinagracia895@gmail.com>
2026-01-15 09:53:22 +0000

    mattre une image ds housing

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -4,7 +4,16 @@ import {HousingLocationInfo} from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
   imports: [],
-  template: '<p>housing-location works!</p>',
+  template: '<section class="listing">\n' +
+    '      <img\n' +
+    '        class="listing-photo"\n' +
+    '        [src]="housingLocation().photo"\n' +
+    '        alt="Exterior photo of {{ housingLocation().name }}"\n' +
+    '        crossorigin\n' +
+    '      />\n' +
+    '      <h2 class="listing-heading">{{ housingLocation().name }}</h2>\n' +
+    '      <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>\n' +
+    '    </section>',
   styleUrl: './housing-location.css',
 })
 export class HousingLocation {
